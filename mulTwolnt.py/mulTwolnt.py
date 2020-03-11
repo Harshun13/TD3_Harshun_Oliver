a8136dc867ca508041d4ffa0d201ca415155c4d7
monty <montyoliver12@hotmail.com>
2020-03-11 19:33:08 +0400

    demande a utilisateur

--- mulTwolnt.py/mulTwolnt.py
+++ mulTwolnt.py/mulTwolnt.py
@@ -1,16 +1,29 @@
 import sys
 
-def add (x,y):
+def add ():
 	return x+y
 
 if __name__=="__main__":
-	if (len(sys.argv) > 3):
-		print("error!! Affichez 2 variable")
-	elif (len(sys.argv) < 3):
-		print("error!! Affichez 2 varaible")
+	if ( len(sys.argv) >  3):
+                print("error!! Entrez que 2 variable")
+
+	elif ( len(sys.argv) == 1 ):
+		x = input("premier valeur: ")
+		y = input("deuxieme valeur: ")
+		x = int(x)
+		y = int(y)
+		print( add() )
+	elif (len(sys.argv) == 2 ):
+		print("error!! Entrez encore une valeur")
+		y = input("Le deuxieme valeur: ")
+		x = int( sys.argv[1] )
+		y = int(y)
+		print( add() )
 	else:
 		x = int( sys.argv[1] )
 		y = int( sys.argv[2] )
-		print(add(x,y))
+		print( add() )
+
+
 
 
