7efe2736cc3872a0223a45ee95d81b7ca86d2069
Harshun13 <seewoogolamsanveer@gmail.com>
2020-03-11 13:21:31 +0400

    message erreur

--- addTwoInt.py/addTwoInt.py
+++ addTwoInt.py/addTwoInt.py
@@ -1,4 +1,19 @@
+import sys
+
 def add (x,y):
-        return x+y
-if  __name__=="__main__":
-        print(add(10,20))
+	return x+y
+
+if __name__=="__main__":
+	if (len(sys.argv) > 3):
+		print("error!! Affichez 2 variable")
+	elif (len(sys.argv) < 3):
+		print("error!! Affichez 2 varaible")
+	else:
+		x = int( sys.argv[1] )
+		y = int( sys.argv[2] )
+		print(add(x,y))
+
+
+
+
+
