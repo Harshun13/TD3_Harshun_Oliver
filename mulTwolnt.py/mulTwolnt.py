bcde0ee05c1b18cb2d07082c464bdae4cc3111ff
monty <montyoliver12@hotmail.com>
2020-03-11 19:16:28 +0400

    addition de 2 valeur

--- mulTwolnt.py/mulTwolnt.py
+++ mulTwolnt.py/mulTwolnt.py
@@ -1,5 +1,15 @@
-def mul(x,y):
+import sys
+
+def add (x,y):
 	return x+y
-if __name__==¨__main__¨:
-	print(add(2,3))
+
+if __name__=="__main__":
+	if (len(sys.argv) > 3):
+		print("error!! Affichez 2 variable")
+	elif (len(sys.argv) < 3):
+		print("error!! Affichez 2 varaible")
+	else:
+		x = int( sys.argv[1] )
+		y = int( sys.argv[2] )
+		print(add(x,y))
 
